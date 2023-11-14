5ca3e14b55e382b1132668916a91eea695e34c87
Amoo Wale <amoowale@gmail.com>
2023-11-14 12:19:14 +0100

    Initial commit

---
+++ Services/ToDoService.cs
@@ -0,0 +1,90 @@
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using TodoListGrpc.Data;
+using TodoListGrpc.Models;
+
+namespace TodoListGrpc.Services;
+
+public class ToDoService : ToDoIt.ToDoItBase
+{
+    private readonly AppDbContext _dbContext;
+
+    public ToDoService(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public override async Task<CreateToDoResponse> CreateToDo(CreateToDoRequest request, ServerCallContext context)
+    {
+        if(request.Title == string.Empty || request.Description == string.Empty)
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "You must supply a valid object"));
+        var toDoItem = new ToDoItem 
+        {
+            Title = request.Title, Description = request.Description
+        };
+        // add to db 
+        await _dbContext.AddAsync(toDoItem);
+        await _dbContext.SaveChangesAsync();
+        return await Task.FromResult(new CreateToDoResponse 
+        {
+            Id = toDoItem.Id
+        });
+    }
+
+    public override async Task<ReadToDoResponse> ReadToDo(ReadToDoRequest request, ServerCallContext context)
+    {
+        if(request.Id <= 0)
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "resource index must be greater than 0"));
+        var toDoItem = await _dbContext.ToDoItems.FirstOrDefaultAsync(t => t.Id == request.Id);
+        if(toDoItem != null)
+            return await Task.FromResult(new ReadToDoResponse{
+                Id = toDoItem.Id, Title = toDoItem.Title, Description = toDoItem.Description, ToDoStatus = toDoItem.ToDoStatus
+            });
+        throw new RpcException(new Status(StatusCode.NotFound, $"No task with id {request.Id}"));
+    }
+
+    public override async Task<GetAllResponse> ListToDo(GetAllRequest request, ServerCallContext context)
+    {
+        var response = new GetAllResponse();
+        var toDoItems = await _dbContext.ToDoItems.ToListAsync();
+        foreach(var toDo in toDoItems)
+        {
+            response.ToDo.Add(new ReadToDoResponse{
+                Id = toDo.Id, Title = toDo.Title, Description = toDo.Description, ToDoStatus = toDo.ToDoStatus
+            });
+        }
+        return await Task.FromResult(response);
+    }
+
+    public override async Task<UpdateToDoResponse> UpdateToDo (UpdateToDoRequest request, ServerCallContext context)
+    {
+        if(request.Id <= 0 || request.Title == string.Empty || request.Description == string.Empty)
+            throw new RpcException(new Status(StatusCode.NotFound, "You must supply a valid object"));
+        var toDoItem = await _dbContext.ToDoItems.FirstOrDefaultAsync(t => t.Id == request.Id);
+        if(toDoItem == null)
+            throw new RpcException(new Status(StatusCode.NotFound, $"No task with id {request.Id}"));
+
+        toDoItem.Title = request.Title;
+        toDoItem.Description = request.Description;
+        toDoItem.ToDoStatus = request.ToDoStatus;
+        await _dbContext.SaveChangesAsync();
+        return await Task.FromResult(new UpdateToDoResponse{
+            Id = toDoItem.Id
+        });
+    }
+
+    public override async Task<DeleteToDoResponse> DeleteToDo (DeleteToDoRequest request, ServerCallContext context)
+    {
+        if(request.Id <= 0)
+            throw new RpcException(new Status(StatusCode.NotFound, "You must supply a valid resource id"));
+        var toDoItem = await _dbContext.ToDoItems.FirstOrDefaultAsync(t => t.Id == request.Id);
+        if(toDoItem == null)
+            throw new RpcException(new Status(StatusCode.NotFound, $"No task with id {request.Id}"));
+        _dbContext.Remove(toDoItem);
+        await _dbContext.SaveChangesAsync();
+        return await Task.FromResult(new DeleteToDoResponse{
+            Id = toDoItem.Id
+        });
+    }
+
+}
